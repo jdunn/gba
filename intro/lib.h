5f4619768ff17ac9bcb2f5a09f4ace64bf76a818
Joshua Dunn <joshua.dunn@gatech.edu>
2011-08-14 19:52:39 -0400

    Slight revision to basic template

--- intro/lib.h
+++ intro/lib.h
@@ -11,9 +11,13 @@ typedef unsigned short u16;
  * NOTE: Declared as extern since definitions usually do not go in header
  * files, causes multiple definition errors (split up in lib.c)
  */
-extern u16 * videoBuffer;
-/* #define videoBuffer         ((u16 *) 0x6000000) */
 
+/* // extern u16 * videoBuffer;
+ * Place the line below in lib.c if you chose to use a variable for videobuffer
+ * // u16 * videoBuffer = (u16 *) 0x6000000;
+ */
+
+#define videoBuffer         ((u16 *) 0x6000000)
 #define REG_DISPCNT         *(u16 *) 0x4000000
 #define MODE_3              3
 #define BG2                 (1 << 10)
@@ -22,9 +26,11 @@ extern u16 * videoBuffer;
 #define COLS                240
 #define COLOR(r, g, b)      (r) | (g) << 5 | (b) << 10
 #define OFFSET(row, col)    (col) + (row) * COLS
+#define PIXEL(r, c, color)  videoBuffer[OFFSET(r,c)] = color
 
 /* Function prototypes */
 void gbaInitialize(void);
+void placePixel(int r, int c, u16 color);
 
 #endif
 
--- intro/main.c
+++ intro/main.c
@@ -2,6 +2,8 @@
  * Basic template for the Game Boy Advance 
  * Mode 3 dev
  */
+
+#include <stdio.h>
 #include "lib.h"
 
 int main(int argc, char * argv[]) {
@@ -9,6 +11,10 @@ int main(int argc, char * argv[]) {
     /* Initialize GBA memory */
     gbaInitialize();
     
+    /* Place a red pixel on the screen */
+    //placePixel(20, 20, COLOR(31,0,0));    // function
+    PIXEL(20, 20, COLOR(0,31,0));           // macro
+
     return 0;
 }
 
