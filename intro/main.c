1e6761852a042287422db3c8c237e5ac05f51eb8
Joshua Dunn <joshua.dunn@gatech.edu>
2011-08-15 15:23:09 -0400

    Added text display

--- intro/main.c
+++ intro/main.c
@@ -1,6 +1,6 @@
 /*
  * Basic template for the Game Boy Advance 
- * Mode 3 dev
+ * Mode 3 dev http://www.coranac.com/tonc/text/
  * main.c
  */
 
@@ -9,6 +9,7 @@
 #include "text.h"
 
 int main(int argc, char * argv[]) {
+    int r, c, ch;
 
     /* Initialize GBA memory */
     gbaInitialize();
@@ -18,7 +19,15 @@ int main(int argc, char * argv[]) {
     //PIXEL(20, 20, COLOR(0,31,0));           // macro
 
     /* Draw text on the screen */
-    drawString(5, 5, "Hello, World!", COLOR(31,31,31));
+    //drawString(5, 5, "Hello, World!", COLOR(31,31,31));
+    for(r = 0, c = 0, ch = 0; ch < 256; ++ch) {
+        if(ch % 25 == 0) {
+            r += 9;
+            c = 5;
+        }
+        c += 7;
+        drawChar(r, c, ch, COLOR(ch % 32, 31, 31));
+    }
 
     return 0;
 }
